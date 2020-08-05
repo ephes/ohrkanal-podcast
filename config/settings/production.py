a502b68d7c0708fd9db84fd4bf23454d1ec60d42
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-08-05 15:50:16 +0200

    collectfast

--- config/settings/production.py
+++ config/settings/production.py
@@ -81,9 +81,10 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "ohrkanal.utils.storages.StaticRootS3Boto3Storage"
-COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
-STATIC_URL = f"https://{aws_s3_domain}/static/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = "ohrkanal.utils.storages.StaticRootS3Boto3Storage"
+#COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
+#STATIC_URL = f"https://{aws_s3_domain}/static/"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "ohrkanal.utils.storages.MediaRootS3Boto3Storage"
